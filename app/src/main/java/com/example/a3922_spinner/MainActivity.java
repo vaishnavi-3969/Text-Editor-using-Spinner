71cf2720c4358504b79e358c417834576094e617
ccoew <vaishnavi.a.kale@cumminscolleg.ein>
2022-09-13 08:43:23 +0530

    updated

--- app/src/main/java/com/example/a3922_spinner/MainActivity.java
+++ app/src/main/java/com/example/a3922_spinner/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a3922_spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Spannable;
@@ -27,22 +28,125 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         editText = findViewById(R.id.mainText);
         Spinner spinner1 = findViewById(R.id.spinnerWheelColor);
         Spinner spinner2 = findViewById(R.id.spinnerWheelAlignment);
-        Spinner spinner3 = findViewById(R.id.spinnerWheelFont);
+        Spinner spinner3 = findViewById(R.id.spinnerWheelSize);
         sp1 = findViewById(R.id.spinnerWheelColor).toString();
         sp2 = findViewById(R.id.spinnerWheelAlignment).toString();
-        sp3 = findViewById(R.id.spinnerWheelFont).toString();
+        sp3 = findViewById(R.id.spinnerWheelSize).toString();
+        
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.Colors, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.Fonts, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.Styles, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
-        spinner1.setOnItemSelectedListener(this);
+//        spinner1.setOnItemSelectedListener(this);
+        spinner1.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                        sp1 = spinner1.getSelectedItem().toString();
+                        switch (sp1){
+                            case "Black":
+                                editText.setTextColor(Color.BLACK);
+                                break;
+                            case "Yellow":
+                                editText.setTextColor(Color.YELLOW);
+                                break;
+                            case "Red":
+                                editText.setTextColor(Color.RED);
+                                break;
+                            case "Blue":
+                                editText.setTextColor(Color.BLUE);
+                                break;
+                            case "Green":
+                                editText.setTextColor(Color.GREEN);
+                                break;
+                            case "Magenta":
+                                editText.setTextColor(Color.MAGENTA);
+                                break;
+                            case "Cyan":
+                                editText.setTextColor(Color.CYAN);
+                        }
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
-        spinner2.setOnItemSelectedListener(this);
+        spinner2.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                        sp2 = spinner2.getSelectedItem().toString();
+                        SpannableStringBuilder spannableString;
+                        switch (sp2) {
+                            case "left":
+                            editText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+                            spannableString = new SpannableStringBuilder(editText.getText());
+                            editText.setText(spannableString);
+                            break;
+                            case "centre":
+                                editText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                                spannableString = new SpannableStringBuilder(editText.getText());
+                                editText.setText(spannableString);
+                                break;
+                            case "right":
+                                editText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+                                spannableString = new SpannableStringBuilder(editText.getText());
+                                editText.setText(spannableString);
+                                break;
+                        }
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter3);
-        spinner3.setOnItemSelectedListener(this);
+        spinner3.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                        sp3 = spinner3.getSelectedItem().toString();
+                        String spannableString;
+                        switch (sp3) {
+                            case "12":
+                                editText.setTextSize(12);
+                                break;
+                            case "14":
+                                editText.setTextSize(14);
+                                break;
+                            case "16":
+                                editText.setTextSize(16);
+                                break;
+                            case "18":
+                                editText.setTextSize(18);
+                                break;
+                            case "20":
+                                editText.setTextSize(20);
+                                break;
+                            case "22":
+                                editText.setTextSize(22);
+                                break;
+                            case "24":
+                                editText.setTextSize(24);
+                                break;
+                            case "26":
+                                editText.setTextSize(26);
+                                break;
+                            case "28":
+                                editText.setTextSize(28);
+                                break;
+                            case "30":
+                                editText.setTextSize(30);
+                                break;
+                        }
+                    }
+
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+//        Spinner spinnerText1 = (Spinner)findViewById(R.id.spinnerWheelColor);
+//        String text1 = spinner1.getSelectedItem().toString();
+//        Spinner spinnerText2 = (Spinner)findViewById(R.id.spinnerWheelAlignment);
+//        String text2 = spinner2.getSelectedItem().toString();
+//        Spinner spinnerText3 = (Spinner)findViewById(R.id.spinnerWheelSize);
+//        String text3 = spinner2.getSelectedItem().toString();
     }
 
     @Override
@@ -83,24 +187,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         editText.setText(spannableString);
     }
-    public void buttonAlignment(View view){
-        Spannable spannableString;
-        switch (sp1){
-            case "left":
-                editText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
-                 spannableString = new SpannableStringBuilder(editText.getText());
-                editText.setText(spannableString);
-                break;
-            case "centre":
-                editText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                 spannableString = new SpannableStringBuilder(editText.getText());
-                editText.setText(spannableString);
-                break;
-            case "right":
-                editText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
-                spannableString = new SpannableStringBuilder(editText.getText());
-                editText.setText(spannableString);
-                break;
-        }
-    }
+
 }
